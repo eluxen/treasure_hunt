4f94a2d92e6e080187c3f44316190c8da228b9a7
Hagai Levin <hagai.levin@klarna.com>
2014-11-02 14:46:54 +0200

    Add NFNetworking FO REAL

---
+++ Pods/Target Support Files/Pods-TreasureHuntProject-AFNetworking/Pods-TreasureHuntProject-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TreasureHuntProject_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_TreasureHuntProject_AFNetworking
+@end
---
+++ Pods/Target Support Files/Pods-TreasureHuntProject/Pods-TreasureHuntProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TreasureHuntProject : NSObject
+@end
+@implementation PodsDummy_Pods_TreasureHuntProject
+@end
