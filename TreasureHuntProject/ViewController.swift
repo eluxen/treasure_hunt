595ca664bc7738fa1c515e84da50f3c82ef36e96
Tomer Levi <Tomer.Levi@Klarna.com>
2014-11-02 11:06:33 +0200

    added push segue to main view controller

--- TreasureHuntProject/ViewController.swift
+++ TreasureHuntProject/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gameName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,17 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func playButtonPressed(sender: UIButton) {
+        if gameName.text.isEmpty {
+            UIAlertView(title: "Treasure Hunt", message: "Please Insert Game Name", delegate: nil, cancelButtonTitle: "OK").show()
+            return;
+        }
+        // get game contents from server
+        
+        // push main view controller
+        var storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        var mainViewController : MainViewController = storyboard.instantiateViewControllerWithIdentifier("MainViewController") as MainViewController
+        self.navigationController?.pushViewController(mainViewController, animated: true)
+    }
 }
 
