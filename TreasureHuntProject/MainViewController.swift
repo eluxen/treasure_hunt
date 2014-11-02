9c55f37358064051390c105dfebddd53401e8798
Tomer Levi <Tomer.Levi@Klarna.com>
2014-11-02 11:31:31 +0200

    added basic map functionality

--- TreasureHuntProject/MainViewController.swift
+++ TreasureHuntProject/MainViewController.swift
@@ -8,28 +8,119 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+import CoreLocation
 
+import MapKit
+
+
+
+class MainViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    
+    @IBOutlet weak var theMap: MKMapView!
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    
+    
+    var manager:CLLocationManager!
+    
+    var myLocations: [CLLocation] = []
+    
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
+        
+        //Setup our Location Manager
+        
+        manager = CLLocationManager()
+        
+        manager.delegate = self
+        
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        manager.requestAlwaysAuthorization()
+        
+        manager.startUpdatingLocation()
+        
+        
+        
+        //Setup our Map View
+        
+        theMap.delegate = self
+        
+        theMap.mapType = MKMapType.Standard
+        
+        theMap.showsUserLocation = true
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    func locationManager(manager:CLLocationManager, didUpdateLocations locations:[AnyObject]) {
+        
+        theLabel.text = "\(locations[0])"
+        
+        myLocations.append(locations[0] as CLLocation)
+        
+        
+        
+        let spanX = 0.007
+        
+        let spanY = 0.007
+        
+        var newRegion = MKCoordinateRegion(center: theMap.userLocation.coordinate, span: MKCoordinateSpanMake(spanX, spanY))
+        
+        theMap.setRegion(newRegion, animated: true)
+        
+        
+        
+        if (myLocations.count > 1){
+            
+            var sourceIndex = myLocations.count - 1
+            
+            var destinationIndex = myLocations.count - 2
+            
+            
+            
+            let c1 = myLocations[sourceIndex].coordinate
+            
+            let c2 = myLocations[destinationIndex].coordinate
+            
+            var a = [c1, c2]
+            
+            var polyline = MKPolyline(coordinates: &a, count: a.count)
+            
+            theMap.addOverlay(polyline)
+            
+        }
+        
     }
-    */
-
+    
+    
+    
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        
+        
+        
+        if overlay is MKPolyline {
+            
+            var polylineRenderer = MKPolylineRenderer(overlay: overlay)
+            
+            polylineRenderer.strokeColor = UIColor.blueColor()
+            
+            polylineRenderer.lineWidth = 4
+            
+            return polylineRenderer
+            
+        }
+        
+        return nil
+        
+    }
+    
 }
